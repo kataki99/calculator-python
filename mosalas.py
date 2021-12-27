cf3cba3a7ab1618215dabc57288a2b79e75e11e7
ali mohammad pour <96487649+kataki99@users.noreply.github.com>
2021-12-27 18:28:36 +0330

    Add files via upload

---
+++ mosalas.py
@@ -0,0 +1,8 @@
+a = int(input("zele aval: "))
+b = int(input("zele dovom: "))
+c = int(input("zele sevom : "))
+
+if (a + c )>b and (b+c)>a and (a + b)>c:
+  print("ghabel rasm ")
+else:
+    print("aedad eshtebah gheir ghabel tarsim")
